8fe1ddffd56b9c3e06c6f450c2140b0773b2e96b
Rkovl <ryan.kovlaske@gmail.com>
2022-11-30 12:16:00 -0800

    Using material design to polish app

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,30 +18,50 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateButton.setOnClickListener{
+        binding.calculateButton.setOnClickListener {
             calculateTip()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keycode, _ ->
+            handleKeyEvent(
+                view,
+                keycode
+            )
+        }
+
     }
 
-    private fun calculateTip(){
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    private fun calculateTip() {
         val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
-        if (cost == null || cost == 0.0){
+        if (cost == null || cost == 0.0) {
             displayTip(0.0)
             return
         }
-        val tipPercent = when (binding.tipOptions.checkedRadioButtonId){
+        val tipPercent = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_fifteen_percent -> 0.15
             else -> 0.10
         }
         var tip = tipPercent * cost
-        if(binding.roundUpSwitch.isChecked){
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
         displayTip(tip)
 
     }
-    private fun displayTip(tip : Double) {
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
